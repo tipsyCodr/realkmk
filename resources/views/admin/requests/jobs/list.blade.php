83b276f992fc5ad3aab79f7f357a73d6e230f6f2
Hemant Kumar <itshemant566@gmail.com>
2024-09-18 11:16:41 +0530

    sad

--- resources/views/admin/requests/jobs/list.blade.php
+++ resources/views/admin/requests/jobs/list.blade.php
@@ -3,8 +3,8 @@
         @foreach ($requests as $request)
             <div class="bg-white rounded-lg shadow p-4">
                 <h5 class="text-xl font-bold">{{ $request->name }}</h5>
-                <p class="text-gray-600">Category: {{ $request->category->name }}</p>
-                <p class="text-gray-600">Category Type: {{ $request->categoryType->name }}</p>
+                {{-- <p class="text-gray-600">Category: {{ $request->category->name }}</p>
+                <p class="text-gray-600">Category Type: {{ $request->categoryType->name }}</p> --}}
                 <p class="text-gray-600">City: {{ $request->city->name }}</p>
                 <p class="text-gray-600">State: {{ $request->state->name }}</p>
                 <p class="text-gray-600">Created At: {{ $request->created_at->format('d-m-Y H:i:s') }}</p>
