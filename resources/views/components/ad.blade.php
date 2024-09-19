2296605bb681f70d1d414dfb63ee8acd36abc0be
Hemant Kumar <itshemant566@gmail.com>
2024-09-19 12:06:33 +0530

    asdsad

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -1,10 +1,8 @@
-<div class="border p-2 flex justify-center  ">
-    @if (!is_null($ad) && isset($ad['url']))
+<div class="border p-2 flex justify-center">
+    @if (!is_null($ad) && isset($ad['url'], $ad['image']))
         <a href="{{ $ad['url'] }}" target="_blank">
             <img class="img-fluid" src="{{ asset('storage/uploads/ad_images/' . $ad['image']) }}"
                 alt="{{ $ad['image'] }}">
         </a>
     @endif
-
-
 </div>
