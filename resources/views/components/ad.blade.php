758336e4f4df1b35f0c45a7695fb7588cd376a52
Hemant Kumar <itshemant566@gmail.com>
2024-09-19 12:05:31 +0530

    sdfa

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -1,5 +1,10 @@
 <div class="border p-2 flex justify-center  ">
-    <a href="{{ $ad['url'] }}" target="_blank">
-        <img class="img-fluid" src="{{ asset('storage/uploads/ad_images/' . $ad['image']) }}" alt="{{ $ad['image'] }}">
-    </a>
+    @if (!is_null($ad) && isset($ad['url']))
+        <a href="{{ $ad['url'] }}" target="_blank">
+            <img class="img-fluid" src="{{ asset('storage/uploads/ad_images/' . $ad['image']) }}"
+                alt="{{ $ad['image'] }}">
+        </a>
+    @endif
+
+
 </div>
