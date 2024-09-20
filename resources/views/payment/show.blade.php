6bc7f3b5d734e8f0e289d7d108b52f56f9746d19
Hemant Kumar <itshemant566@gmail.com>
2024-09-20 22:42:08 +0530

    asd

--- resources/views/payment/show.blade.php
+++ resources/views/payment/show.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
-    <div class="wrapper text-center">
+    <div class="flex flex-col justify-center items-center wrapper text-center">
         <h1 class="text-xl font-bold pt-6">Make Payment to Continue </h1>
         <p class="text-3xl  font-bold">Rs. {{ $amount }}</p>
         <p class="text-lg text-blue-400">Scan this QR Code to make Payment.</p>
-        <img src="{{ asset('img/pay.jpg') }}" alt="">
+        <img class="border p-2 w-2/3  mt-8" src="{{ asset('img/pay.jpg') }}" alt="">
     </div>
 </x-app-layout>
