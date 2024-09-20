ad9b5e68d45353c27793a83c36a9e2a67795ff83
Hemant Kumar <itshemant566@gmail.com>
2024-09-20 22:52:45 +0530

    sd

--- resources/views/payment/show.blade.php
+++ resources/views/payment/show.blade.php
@@ -4,6 +4,10 @@
         <p class="text-3xl  font-bold">Rs. {{ $amount }}</p>
         <p class="text-md font-bold text-blue-400">Scan this QR Code below to make Payment.</p>
 
+        <a href="{{ asset('img/pay.jpg') }}" download="qrcode.jpg"
+            class="text-white bg-blue-500 p-2 rounded-md mt-4">Download QR Code</a>
+
         <img class="border p-2 w-2/3  mt-8" src="{{ asset('img/pay.jpg') }}" alt="">
+
     </div>
 </x-app-layout>
