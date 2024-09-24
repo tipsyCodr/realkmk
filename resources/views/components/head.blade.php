2c88f8fb9739c31b05e10606f1ff30c2e344c57a
Hemant Kumar <itshemant566@gmail.com>
2024-09-24 23:46:40 +0530

    added post button on head

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -14,6 +14,10 @@
             </a>
         </div>
         <div class="text-center my-2 self-center flex flex-row place-items-center px-2">
+            <a href="{{ route('listing.types', 'properties') }}"
+                class="p-1 mx-2 capitalize text-xs flex items-center bg-yellow-200 border rounded text-yellow-900 border-yellow-500  ">Post
+                a free
+                ad <i class="fa-solid fa-plus-square fa-2x pl-2 text-yellow-500"></i></a>
             {{-- <x-location-icon />
             <p><u>{{ $location }}</u></p> --}}
             <a href="{{ route('login') }}">
