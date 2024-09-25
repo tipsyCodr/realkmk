ac7d83ef3ae927ff1e852d087ff6198dddb524ce
Hemant Kumar <itshemant566@gmail.com>
2024-09-25 08:17:51 +0530

    ad on every 6th post

--- resources/views/components/post-iterator.blade.php
+++ resources/views/components/post-iterator.blade.php
@@ -7,7 +7,7 @@
     <?php $adCount = 0; ?>
     @foreach ($posts as $post)
         <x-post-item :post="$post" />
-        @if ($loop->iteration == 4 || $loop->iteration % 4 == 0)
+        @if ($loop->iteration == 2 || $loop->iteration % 6 == 0)
             <?php $adCount++; ?>
             <!-- Show ad here -->
             <div class="col-span-2 sm:col-span-1 md:col-span-1 xl:col-span-1">
