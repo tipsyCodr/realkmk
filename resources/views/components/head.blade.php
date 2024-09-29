82469cea84417946ac5ee61246914f9667557fca
Hemant Kumar <itshemant566@gmail.com>
2024-09-29 13:54:01 +0530

    sd

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -11,8 +11,8 @@
         </div>
         <div class="text-center my-2 self-center flex flex-row place-items-center px-2 ">
             <a href="{{ route('listing.types', 'properties') }}"
-                class="text-right scale-75 p-1 mx-2 capitalize text-xs flex items-center bg-yellow-200 border rounded text-yellow-900 border-yellow-500  ">Post
-                a <br>free
+                class="text-right sm:scale-100 scale-75 p-1 mx-2 capitalize text-xs flex items-center bg-yellow-200 border rounded text-yellow-900 border-yellow-500  ">Post
+                a free
                 ad <i class="fa-solid fa-plus-square fa-2x pl-2 text-yellow-500"></i></a>
 
             <a href="{{ route('login') }}">
