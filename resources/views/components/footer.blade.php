08df70f259b3ba6032cbf38981cff9324c30491c
Hemant Kumar <itshemant566@gmail.com>
2024-09-29 13:52:03 +0530

    Added Ad on eveery page

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,21 @@
 <footer style="text-align: center;">
     <footer>
+        <div class="ads w-full flex justify-center items-center">
+            @php
+                $ads = \App\Models\Ad::inRandomOrder()->first();
+            @endphp
+            @if ($ads)
+                <a class="block max-w-xl w-full mx-4 mb-4 flex justify-center" href="{{ $ads->url }}" target="_blank">
+                    <img class="mx-auto"
+                        src="{{ asset('storage/uploads/ad_images/' . $ads->location . '/' . $ads->location . '_' . $ads->image) }}" />
+                </a>
+            @endif
+        </div>
         <div class="environment">
             <p>Copyright © 2024 RealKMK. All rights reserved.</p>
         </div>
 
-        <div class="copyrights">
+        <div class="copyrights mb-32">
             <p>Designed and developed by <a href="https://www.facebook.com/vshlkumar566">Hemant Kumar</a>
             </p>
         </div>
