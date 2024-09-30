516059d5082c524de23db9b66d63fcfbe74bd612
Hemant Kumar <itshemant566@gmail.com>
2024-09-30 11:46:28 +0530

    Added ad header

--- resources/views/components/ad.blade.php
+++ resources/views/components/ad.blade.php
@@ -1,4 +1,5 @@
 <div class="border p-2 flex justify-center">
+    <p class='text-xs text-gray-600 px-1'>Banner Advertisement</p>
     @if (!is_null($ad) && isset($ad['url'], $ad['image']))
         <a href="{{ $ad['url'] }}" target="_blank">
             <img class="img-fluid"
