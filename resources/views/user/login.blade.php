a5ebdf8d4a0ac6c02967a33b5c9cab799e538d96
Hemant Kumar <itshemant566@gmail.com>
2024-09-30 21:58:20 +0530

    added password peeker

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -91,7 +91,7 @@
                         type="text" />
                 </div>
 
-                <div class="mt-4">
+                <div class="mt-4 relative">
                     <div class="flex justify-between">
                         <label class="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
                             for="loggingPassword">Password</label>
@@ -102,6 +102,10 @@
                     <input id="loggingPassword" name="password"
                         class="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
                         type="password" />
+
+                    <button type="button" id="togglePassword" class="absolute right-4 top-9 text-gray-500">
+                        <i class='fa fa-eye hover:text-gray-300 transition-colors'></i>
+                    </button>
                 </div>
 
                 <div class="mt-6">
@@ -217,6 +221,18 @@
                     console.error("Error during sign-in:", error.message);
                     alert("Authentication failed: " + error.message);
                 });
+        });
+
+        const passwordInput = document.getElementById('loggingPassword');
+        const togglePasswordButton = document.getElementById('togglePassword');
+
+        togglePasswordButton.addEventListener('click', function() {
+            const currentType = passwordInput.getAttribute('type');
+            if (currentType === 'password') {
+                passwordInput.setAttribute('type', 'text');
+            } else {
+                passwordInput.setAttribute('type', 'password');
+            }
         });
     </script>
 </x-login-layout>
