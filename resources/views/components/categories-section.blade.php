6a655315b8d0d56c1ea1fcc337846d1d918b7313
Hemant Kumar <itshemant566@gmail.com>
2024-10-05 21:45:49 +0530

    sd

--- resources/views/components/categories-section.blade.php
+++ resources/views/components/categories-section.blade.php
@@ -1,6 +1,6 @@
 <div class="pb-2 bg-gray-200">
     <div class=' px-2 bg-white'>
-        <p>Browse Categories</p>
+        {{-- <p>Browse Categories</p> --}}
         <div class=" item-wrapper item flex flex-row justify-center overflow-x-scroll ">
             <a href="{{ route('properties') }}"
                 class=" h-auto w-auto flex flex-col items-center p-2 m-1 border rounded hover:bg-gray-50 transition-color ">
