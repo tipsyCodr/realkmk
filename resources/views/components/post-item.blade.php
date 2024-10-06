9640e656dd6e59f6baabf0793e9af5e501603d58
Hemant Kumar <itshemant566@gmail.com>
2024-10-06 08:42:03 +0530

    fixed post

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,21 +1,21 @@
 <a class="" href="{{ route('listing.view', $post['id']) }}">
     <div class="mx-auto px-2 my-6 ">
         <div class="max-w-xs cursor-pointer rounded-lg bg-white p-2 shadow duration-150 hover:scale-105 hover:shadow-md">
-            <div class="h-[250px]">
+            <div class="h-[200px] bg-gray-500">
                 @php
                     $photos = json_decode($post['photos'], true);
                 @endphp
                 @if (is_array($photos))
                     @foreach ($photos as $photo)
                         <img src="{{ $photo ? Storage::url($photo) : 'https://placehold.co/300x400?text=No\nImage' }}"
-                            alt="{{ $post['title'] }}"
+                            alt="{{ $post['title'] }}" style='height: 200px;'
                             class="w-full h-[250px] rounded-lg object-contain object-center {{ $photo ? '' : 'bg-gray-300' }}"
                             alt="listing" onerror="this.src='https://placehold.co/300x400?text=No\nImage'">
                     @break
                 @endforeach
             @else
                 <img src="{{ $post['photos'] ? Storage::url('uploads/property_images/' . $post['photos']) : 'https://placehold.co/300x400?text=No\nImage' }}"
-                    alt="{{ $post['title'] }}"
+                    alt="{{ $post['title'] }}" style='height: 200px;'
                     class="w-full h-[250px] rounded-lg object-contain object-center {{ $post['photos'] ? '' : 'bg-gray-300' }}"
                     alt="listing" onerror="this.src='https://placehold.co/300x400?text=No\nImage'">
             @endif
