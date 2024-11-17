ee4ac9e59c3f49d1125b65e78ab30bd597acfea5
Hemant Kumar <itshemant566@gmail.com>
2024-11-17 11:07:23 +0530

    changed back button color

--- resources/views/components/back-button.blade.php
+++ resources/views/components/back-button.blade.php
@@ -1,7 +1,7 @@
 <!-- It is quality rather than quantity that matters. - Lucius Annaeus Seneca -->
 <div class="p-2">
     <button
-        class="bg-yellow-300 hover:bg-yellow-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+        class="bg-blue-100 hover:bg-blue-200 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
         onclick="history.back()">
         <i class="fa  fa-arrow-left "></i> Back
     </button>
