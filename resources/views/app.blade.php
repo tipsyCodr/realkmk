ffad80a21560021fb526fda67c5ba3539a5de0bc
Hemant Kumar <itshemant566@gmail.com>
2025-01-07 08:16:36 +0530

    failed home

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,6 +1,7 @@
 <x-app-layout>
     {{-- <x-search-bar /> --}}
-    <div class="bg-blue-100 text-center font-semibold border mx-2 mt-20 capitalize border-blue-500 text-black p-2" role="alert">
+    <div class="hidden bg-blue-100 text-center font-semibold border mx-2 mt-20 capitalize border-blue-500 text-black p-2"
+        role="alert">
         <p>Buyers can search properties from here</p>
     </div>
     <x-categories-section />
