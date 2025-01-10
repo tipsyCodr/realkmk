4a06a7bfff5e49fc54781c0cad947e21dfce8a17
Hemant Kumar <itshemant566@gmail.com>
2025-01-10 07:14:43 +0530

    temporarily open the site

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,9 @@
-{{-- <x-app-layout>
-    <x-search-bar />
+<x-app-layout>
+    {{-- <x-search-bar /> --}}
     <div class="bg-blue-100 text-center font-semibold border mx-2 mt-20 capitalize border-blue-500 text-black p-2"
         role="alert">
         <p>Buyers can search properties from here</p>
     </div>
     <x-categories-section />
     <x-post-list :posts="$listings" />
-</x-app-layout> --}}
+</x-app-layout>
