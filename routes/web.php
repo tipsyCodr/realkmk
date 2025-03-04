445b3eaed116c97ce80c706e64ef9b2858d57328
Hemant Kumar <itshemant566@gmail.com>
2025-03-04 19:38:56 +0530

    commented route - uncomment to fix

--- routes/web.php
+++ routes/web.php
@@ -74,9 +74,9 @@ Route::middleware('checkUser')->group(function () {
     //Listing Routes
     Route::name('listing.')->prefix('listing')->group(function () {
         Route::get('post', [WebController::class, 'post'])->name('post');
-        // Route::get('post/type/{category}/{role}', [ListingController::class, 'postCategoriesTypes'])->name('types');
-        Route::get('post/type/{category}/{role}/{categoryType}', [ListingController::class, 'postForm'])->name('form');
-        Route::post('post/store', [ListingController::class, 'storePropertyListing'])->name('store');
+        Route::get('post/type/{category}/{role}', [ListingController::class, 'postCategoriesTypes'])->name('types');
+        // Route::get('post/type/{category}/{role}/{categoryType}', [ListingController::class, 'postForm'])->name('form');
+        // Route::post('post/store', [ListingController::class, 'storePropertyListing'])->name('store');
     });
 
     //Jobs Section
