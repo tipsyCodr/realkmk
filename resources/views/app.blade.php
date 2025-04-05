cb9baeb0899d0ef0413d3cf1ae69b7b1519703a6
Hemant Kumar <itshemant566@gmail.com>
2025-04-05 12:50:32 +0530

    downed the site

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,14 @@
-<x-app-layout>
+<div class="p-2 mx-2 mt-4 font-semibold text-center text-black capitalize bg-red-100 border border-red-500"
+    role="alert">
+    <p>This site has been deactivated by the owner. Please complete the payment to restart the service.</p>
+</div>
+
+{{-- <x-app-layout> --}}
     {{-- <x-search-bar /> --}}
-    <div class="bg-blue-100 text-center font-semibold border mx-2 mt-20 capitalize border-blue-500 text-black p-2"
+    {{-- <div class="bg-blue-100 text-center font-semibold border mx-2 mt-20 capitalize border-blue-500 text-black p-2"
         role="alert">
         <p>Buyers can search properties from here</p>
     </div>
     <x-categories-section />
     <x-post-list :posts="$listings" />
-</x-app-layout>
+</x-app-layout> --}}
