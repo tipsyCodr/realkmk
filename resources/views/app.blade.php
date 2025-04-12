ad0cd919cc84d2c85d5dcc4689ce0eeb2449b095
Hemant Kumar <itshemant566@gmail.com>
2025-04-12 22:57:15 +0530

    turned on on grace period

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,14 +1,14 @@
-<div class="p-2 mx-2 mt-4 font-semibold text-center text-black capitalize bg-red-100 border border-red-500"
+{{-- <div class="p-2 mx-2 mt-4 font-semibold text-center text-black capitalize bg-red-100 border border-red-500"
     role="alert">
     <p>This site has been deactivated by the owner. Please complete the payment to restart the service.</p>
-</div>
+</div> --}}
 
-{{-- <x-app-layout> --}}
-    {{-- <x-search-bar /> --}}
-    {{-- <div class="bg-blue-100 text-center font-semibold border mx-2 mt-20 capitalize border-blue-500 text-black p-2"
+<x-app-layout>
+    <x-search-bar />
+    <div class="p-2 mx-2 mt-20 font-semibold text-center text-black capitalize bg-blue-100 border border-blue-500"
         role="alert">
         <p>Buyers can search properties from here</p>
     </div>
     <x-categories-section />
     <x-post-list :posts="$listings" />
-</x-app-layout> --}}
+</x-app-layout>
